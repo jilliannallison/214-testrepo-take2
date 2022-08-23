e1a44fc35c70c0784d07b0a4979cc9fcd4ce3a34
erica-bishop <erica.nicole.bishop@gmail.com>
2022-08-23 23:05:03 +0000

    dessert matching methods

--- dessert-script-3.R
+++ dessert-script-3.R
@@ -1,19 +1,65 @@
 library(here)
 library(tidyverse)
 library(rvest)
+library(janitor)
+library(sqldf)
 
-our_desserts <- read_csv(here("favorite_desserts.csv"))
+our_desserts <- read_csv(here("favorite_desserts.csv")) %>% 
+  clean_names()
 
 our_desserts <- our_desserts %>% 
-  rename(dessert = Favorite_dessert)
+  rename(dessert = favorite_dessert) %>% 
+  mutate(dessert = tolower(dessert))
 
-iconic_desserts <- read_csv(here("iconic_desserts.csv"))
+iconic_desserts <- read_csv(here("iconic_desserts.csv")) %>% 
+  clean_names() %>% 
+  mutate(dessert = tolower(dessert))
 
-#finding which of our desserts are iconic with a left join:
+#finding which of our desserts are iconic with a semi join:
 
-our_iconic_deserts <- left_join(x = our_desserts, y = iconic_desserts) %>% 
-  replace_na(list(first_name = NA, last_name = NA, dessert = NA, rank = "NOT ICONIC"))
+our_iconic_deserts <- semi_join(x = our_desserts, y = iconic_desserts) %>% 
+  mutate(rank = replace_na("UR ICONIC"))
+
+#find out which desserts are iconic with a for loop
+
+# Test with no matches
+matches <- c()
+
+for (i in seq_along(our_desserts$dessert)) {
+  for (j in seq_along(iconic_desserts$dessert)) {
+    if (our_desserts$dessert[i] == iconic_desserts$dessert[j]) {
+      matches <- append(matches, our_desserts$dessert[i])
+    }
+  }
+}
+
+if (length(matches) == 0) {
+  message("No matches.")
+}
+
+if (length(matches) != 0) {
+  print(matches_test)
+}
+
+# should output No matches.
+
+# Now using the sqldf package
+#are_we_iconic <- sqldf('SELECT * FROM our_desserts INTERSECT SELECT * FROM iconic_desserts')
+#getting Error: SELECTs to the left and right of INTERSECT do not have the same number of result columns
+
+
+#finding iconic desserts with generics::intersect
+
+#matching_desserts <- generics::intersect(x = our_desserts, y = iconic_desserts)
+#Error:`x` and `y` are not compatible. (need to have same number of columns)
 
 
 
+
+#Using filter:
+
+dessert_match2 <- our_desserts %>% 
+  filter(dessert %in% iconic_desserts$dessert)
+
+#try using 
 
